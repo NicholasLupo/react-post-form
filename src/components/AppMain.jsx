47373f84e2c5d65276e25ace76e28c568441d51d
NicholasLupo <nicholaslupo14@gmail.com>
2026-05-08 14:55:29 +0200

    add form and input

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,7 +4,50 @@ export default function AppMain() {
 
     return (
         <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 mt-4">
+                        <h1 className="mb-4"> Crea il tuo post! </h1>
+                        <form>
+                            <div className="row g-3 row-cols-1">
+                                <div className="col">
+                                    <input
+                                        className="form-control"
+                                        placeholder="Inserisci il nome dell'autore del blog"
+                                        name="author"
+                                        type="text"
+                                    /* value={}
+                                    onChange={} */
+                                    />
+                                </div>
+                                <div className="col">
+                                    <input
+                                        className="form-control"
+                                        placeholder="Inserisci il titolo del blog"
+                                        name="title"
+                                        type="text"
+                                    /* value={ }
+                                    onChange={ } */
+                                    />
+                                </div>
+                                <div className="col">
+                                    <input
+                                        className="form-control"
+                                        placeholder="Inserisci il contenuto del blog"
+                                        name="content"
+                                        type="text"
+                                    /* value={ }
+                                    onChange={ } */
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-md-4">
 
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
